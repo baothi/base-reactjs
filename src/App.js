009cd31e5cef7886fb1ade6a879067172c8422eb
baothi <baothi246@gmail.com>
2022-10-10 07:08:18 +0700

    create menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
+import Header from './components/Header/Header';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+// import MyComponent from './components/MyComponent';
+// import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-container'>
-      hello world with baothi &amp; hoc reactjs
-      <MyComponent />
-    </div>
-    )
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className='app-container'>
+//       hello world with baothi &amp; hoc reactjs
+//       <MyComponent />
+//     </div>
+//     )
+//   }
+// }
 
 // function App() {
 //   const count = useSelector(state => state.counter.count);
@@ -41,4 +42,12 @@ class App extends React.Component {
 //     </div>
 //   );
 // }
+
+const App = () => {
+  return (
+    <div className='app-container'>
+     <Header />
+    </div>
+  )
+};
 export default App;
