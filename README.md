ea620ce34e6f069371d380c7a51f35a364d98828
baothi <baothi246@gmail.com>
2022-10-10 17:57:53 +0700

    create css menu

--- README.md
+++ README.md
@@ -71,3 +71,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 npm install --save-exact sass@1.53.0
 npm install --save-exact react-router@6.3.0 bootstrap@5.2.0 react-bootstrap@2.5.0
 npm install react-router-dom@6.3.0
+npm install --save-exact react-pro-sidebar@0.7.1
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,29 +16,17 @@ const Header = () => {
             <NavLink to="/" className='nav-link'>Home</NavLink>
             <NavLink to="/users" className='nav-link'>Users</NavLink>
             <NavLink to="/admins" className='nav-link'>Admin</NavLink>
-            {/* <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/users">Users</Nav.Link>
-            <Nav.Link href="/admins">Admin</Nav.Link> */}
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
           <Nav>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
+            <button className='btn-login'>Login</button>
+            <button className='btn-signup'>Sign Up</button>
+            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item>Login</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
               <NavDropdown.Item>Separated link</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item>Log Out</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
